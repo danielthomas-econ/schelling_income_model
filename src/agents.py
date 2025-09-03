huge bug fix update. fixed bugs with: 1) neighborhood, houses, rent_paid mapping in the agents array 2) incorrect units in the bid logic 3) made bids proportional to income to prevent rich agents from getting boost in both income and utility 4) incorrect indexing in the check_priced_out function

src/agents.py
@@ -104,7 +104,7 @@ def generate_agents(n_agents=n_agents, mean_income=mean_income, log_std=log_std)
     incomes = np.random.lognormal(mean=mu, sigma=log_std, size=n_agents)
     brackets = np.searchsorted(cutoffs, incomes) - 1 # sorts the incomes into the cutoffs list
     
-    locations = np.random.uniform(0, 10, size=(n_agents, 2))
+    """locations = np.random.uniform(0, 10, size=(n_agents, 2))
     # floor the coords and calculate neighborhood numbers
     floored = np.floor(locations).astype(int)
     x_coords = floored[:, 0]
@@ -112,15 +112,15 @@ def generate_agents(n_agents=n_agents, mean_income=mean_income, log_std=log_std)
     # we have 100 1x1 neighborhoods in a 10x10 grid
     # start from 0-9 on the bottom row, all the way to 90-99 on the top row
     # so every cell number when labelled like this ends with the floor of the x coord and begins with the floor of the y coord
-    neighborhood = y_coords * 10 + x_coords    
-
+    neighborhood = y_coords * 10 + x_coords"""   # old neighborhood assignment system, i think it caused misalignment bugs. anyways eliminating these calculatations should be more efficient? 
+    
     # initialize agents
     agents = np.zeros(n_agents, dtype=agent_dtype)
 
     agents["id"] = np.arange(n_agents) 
     agents["income"] = incomes
     agents["income_bracket"] = brackets
-    agents["neighborhood"] = neighborhood
+    agents["neighborhood"] = np.full(n_agents,-1) # they all start homeless, eventually get allocated as they bid. test this vs the old neighborhood assignment system
     agents["happy"] = False # initially everyone is depressed :(
     agents["house"] = np.full(n_agents,-1)
     agents["rent_paid"] = np.zeros(n_agents)

src/bidding.py
@@ -24,7 +24,7 @@ def get_utilities(agents, proportions):
         rent = agents["rent_paid"][i]
         θ = agents["theta"][i]
         # ------------ disposable income left (should this be rent after moving to k? check the logic here later) ------------ #
-        c = income - rent
+        c = (income - rent)/income # housing burden relative to income
         if c <= 0.0:
             c_term = 0.0 # no utility if rents exceed your income, thats impossible
         else:
@@ -71,7 +71,7 @@ def place_bid(agents, utilities,
         need_to_bid = not(happy[i])
         
         if need_to_bid:
-            utility_bids = β * incomes[i] + λ * utilities[i,:] # utilities of all neighborhoods for agent i
+            utility_bids = (β + λ * utilities[i,:]) * incomes[i] # utilities of all neighborhoods for agent i
             max_bids = δ * incomes[i]
             # a vector of all the potential bids the agent would make for all 100 neighborhoods
             final_bids = np.minimum(utility_bids, max_bids)

src/houses.py
@@ -30,6 +30,7 @@ def initialize_houses(agents):
     return houses
 
 "--------------------------------------- populate the houses column in agents ---------------------------------------"
+@njit(cache = True)
 def agent_house_mapping(agents, houses):
     n_agents = agents.shape[0]
     n_houses = houses.shape[0]
@@ -42,26 +43,24 @@ def agent_house_mapping(agents, houses):
             agents["house"][t] = h
     return
 "--------------------------------- check if an agent can no longer afford their home --------------------------------"
-"""We define the agent's max WTP for rent in neighborhood $j$ as
-$B_{i}^{\text{stay}}= \text{min}(\beta Y_{i} + \lambda Q_i, \delta Y_i)$
-The core logic is identical to bidding, but we had to replace $U$ since that compares two neighborhoods
-We replace it with $Q_{i} = r_{k}$ percent of agents in neighborhood $k$ with >= income to eliminate the comparative nature of $U$."""
 @njit(parallel = True, cache = True)
-def check_priced_out(agents, houses, proportions, β=0.3, λ=0.2, δ=0.4): # removing proportions as an argument causes numba issues
+def check_priced_out(agents, houses, proportions, β=0.3, λ=0.2, δ=0.6): # removing proportions as an argument causes numba issues
     rents = houses["value"] # base it off the current house value, not how much the agent pays for rent
     n_agents = agents.shape[0]
     priced_out_mask = np.zeros(n_agents, dtype=np.bool_)
 
     for i in prange(n_agents):
+        h = agents["house"][i]
+        if h == -1:
+            continue # homeless agents dont have a home they can be priced out of anyways
+        
         nb = agents["neighborhood"][i]
-        if nb == -1: # homeless agents cant be priced out
-            continue
-
         income = agents["income"][i]
         bracket = agents["income_bracket"][i]
         Q_i = proportions[nb, bracket]
-        B_stay = min(β*income + λ*Q_i, δ*income) # priced out logic
-        if rents[i] > B_stay:
+        B_stay = min((β + λ*Q_i)*income, δ*income) # priced out logic
+        rent = houses["value"][h]
+        if rent > B_stay:
             priced_out_mask[i] = True
     
     return priced_out_mask
@@ -76,6 +75,7 @@ def evict_priced_out(agents, houses, priced_out_mask):
                 houses["tenant"][h] = -1 # evict them
                 agents["house"][i] = -1
                 agents["neighborhood"][i] = -1
+                agents["rent_paid"][i] = 0.0 # you don't pay rent if you're homeless
     return
 "---------------------------------------- decide which agent gets which house ---------------------------------------"
 @njit(cache=True)
@@ -115,6 +115,7 @@ def allocate_houses(agents, houses, bids, neighborhood_chosen, β=0.3, λ=0.2, �
             agents["rent_paid"][w] = bids[w]
             agents["neighborhood"][w] = n
 
+    agent_house_mapping(agents, houses) # update the mapping after the allocation is made
     return agents, houses, cutoff_bids
 
 "-------------------------------------- update the house prices based on demand -------------------------------------"