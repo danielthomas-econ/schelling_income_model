fixed the age old utility calculation bug (now c takes rent of the prospective neighborhood into account) and some bookkeeping bugs

src/agents.py
@@ -51,11 +51,11 @@ def find_income_brackets(agents, percentiles = PERCENTILES):
 def get_freq_and_total(agents):
     neighborhoods = agents["neighborhood"]
     income_brackets = agents["income_bracket"]
+    freq = np.zeros((N_NEIGHBORHOODS, N_BRACKETS), dtype = np.int32)
 
-    # 100 -> no. of neighborhoods (0-99), 12 -> no. of income brackets (0-11)
-    freq = np.zeros((100,12), dtype = np.int32)
     # avoid homeless people living in neighborhood -1
     valid = neighborhoods != -1
+
     # takes a (nb,ib) pair as a coordinate and increments freq[nb,ib] by 1
     # directly fills in the freq values, we're done with updating this now
     if valid.any():
@@ -71,11 +71,11 @@ def get_freq_and_total(agents):
 @njit(parallel = True, cache = True)
 def get_proportion(freq, total):
     # precomputes an array to check what proportion in neighborhood j has >= income bracket i
-    proportions = np.zeros((100,12), dtype = np.float32)
+    proportions = np.zeros((N_NEIGHBORHOODS,N_BRACKETS), dtype = np.float32)
     # parallelizing with prange since every nb works on a different row
-    for nb in prange(100): 
+    for nb in prange(N_NEIGHBORHOODS): 
         if total[nb] == 0: # if no agents live there: avoids division by zero errors
-            for ib in range(12):
+            for ib in range(N_BRACKETS):
                 proportions[nb,ib] = 0.0 # no agents => proportions = 0 for every income bracket
             continue
         """# code for the +-1 income bracket logic
@@ -89,7 +89,7 @@ def get_proportion(freq, total):
         # code for the >= income bracket logic
         running_sum = 0 
         # iterate over brackets backwards to get >= bracket count
-        for ib in range(11,-1,-1):
+        for ib in range(N_BRACKETS-1, -1,-1):
             running_sum += freq[nb, ib]
             proportions[nb, ib] = running_sum / total[nb]
     return proportions
@@ -128,23 +128,13 @@ def generate_agents(n_agents = N_AGENTS):
         ("theta", np.float32), # numba doesnt like float16, so we stick to float32 here
     ])
     
-    locations = np.random.uniform(0, 10, size=(n_agents, 2))
-    # floor the coords and calculate neighborhood numbers
-    floored = np.floor(locations).astype(int)
-    x_coords = floored[:, 0]
-    y_coords = floored[:, 1]
-    # we have 100 1x1 neighborhoods in a 10x10 grid
-    # start from 0-9 on the bottom row, all the way to 90-99 on the top row
-    # so every cell number when labelled like this ends with the floor of the x coord and begins with the floor of the y coord
-    neighborhood = y_coords * 10 + x_coords # assigns a neighborhood based on their random x and y coords
-    
     # initialize agents
     agents = np.zeros(n_agents, dtype=agent_dtype)
 
     agents["id"] = np.arange(n_agents) 
     agents["income"] = get_incomes(agents)
     agents["income_bracket"] = find_income_brackets(agents)
-    agents["neighborhood"] = neighborhood
+    agents["neighborhood"] = allocate_neighborhood(agents)
     agents["happy"] = False # initially everyone is depressed :(
     agents["house"] = np.full(n_agents,-1)
     agents["rent_paid"] = np.zeros(n_agents)

src/bidding.py
@@ -5,7 +5,7 @@ from numba import njit, jit, prange
 "------------------------------------------- cobb-douglas utility function ------------------------------------------"
 # using the proportions argument to prevent redoing the math, make sure to update the proportions array appropriately
 @njit(parallel = True, cache = True)
-def get_utilities(agents, proportions):
+def get_utilities(agents, proportions, current_rents):
     # find utility agent i gets from moving to neighborhood k
     n = agents.size
     utilities = np.zeros((n, 100), dtype = np.float32)
@@ -17,19 +17,16 @@ def get_utilities(agents, proportions):
 
         ib = agents["income_bracket"][i]
         income = agents["income"][i]
-        rent = agents["rent_paid"][i]
         θ = agents["theta"][i]
-        # ------------ disposable income left (should this be rent after moving to k? check the logic here later) ------------ #
-        c = (income - rent)/income # housing burden relative to income
-        if c <= 0.0:
-            c_term = 0.0 # no utility if rents exceed your income, thats impossible
-        else:
-            c_term = c ** (1-θ) # avoids recomputation for each neighborhood
 
         row_max = 0.0
         for k in range(100):
+            rent = current_rents[k]
+            c = (income-rent)/income # what % of income is left after moving into the new prospective neighborhood?
+            if c <= 0.0:
+                c = 0.0
             q = proportions[k, ib] # quality of the new neighborhood
-            val = (q ** θ) * c_term
+            val = (q ** θ) * (c ** (1-θ)) # utility fn
             utilities[i, k] = val
             if val > row_max:
                 row_max = val
@@ -48,10 +45,9 @@ def place_bid(agents, utilities,
               λ = LAMBDA, # marginal WTP for 1 unit of social utility U
               δ = DELTA): # max cap on affordability, so that bids dont take up entire agent income
     
-    n_agents, n_neighborhoods = utilities.shape
+    n_agents = agents.size
     happy = agents["happy"]
     incomes = agents["income"]
-    rent_paid = agents["rent_paid"]
     bids = np.zeros(n_agents, dtype = np.float64)
     # which neighborhood the agents chooses to bid for
     # -1 => they're not bidding this round

src/common.py
@@ -1,25 +1,37 @@
 "All the constants that the sim relies on"
+import numpy as np
 
-# population of the sim
+# default population of the sim
 N_AGENTS = 1_000_000
 
-GRID_SIZE = 10 # the length of the square's side
-N_NEIGHBORHOODS = GRID_SIZE**2 # since the grid is a square, we have side^2 neighborhoods
+N_NEIGHBORHOODS = 100 
+
+# percentiles tells us how many income brackets do you want?
+PERCENTILES = [0,10,20,30,40,50,60,70,80,90,95,99,100]
+N_BRACKETS = len(PERCENTILES)-1
+
+# something to consider:
+# i deleted the grid variable for now, but will it be needed when we have to visualize the model?
+# that might restrict n_neighborhoods to being a perfect square
+
+def allocate_neighborhood(agents, n_neighborhoods = N_NEIGHBORHOODS):
+    neighborhoods = np.random.uniform(0,n_neighborhoods, agents.size)
+    return neighborhoods
 
 # what percent of agents must have >= income?
 DEFAULT_HAPPINESS_PERCENT = 0.5
 
+# use as a robustness check: does our result change if the starting house price varies?
+STARTING_HOUSE_PRICE = 1_00_000
+
 # housing price update rules
 DECAY_RATE = 0.95       # fall in price if supply > demand
-MAX_CHANGE = 0.2         # max % change in one round, prevents insane price swings
+MAX_CHANGE = 0.1        # max % change in one round, prevents insane price swings
 
 # agent behavior parameters
-BETA = 0.3               # baseline fraction of income WTP
-LAMBDA = 0.2             # marginal WTP for social utility
-DELTA = 0.6              # maximum income agent can spend on rent
-
-# percentiles tells us how many income brackets do you want?
-PERCENTILES = [0,10,20,30,40,50,60,70,80,90,95,99,100]
+BETA = 0.3              # baseline fraction of income WTP
+LAMBDA = 0.2            # marginal WTP for social utility
+DELTA = 0.6             # maximum income agent can spend on rent
 
 # theta is the preference for neighborhood quality, we'll uniformly distribute it
 THETA_MIN = 0.6

src/houses.py
@@ -4,16 +4,7 @@ from numba import njit, jit, prange
 
 "--------------------------------- create a structured array with all info of houses --------------------------------"
 def initialize_houses(agents):
-    n_agents = agents.shape[0]
-    locations = np.random.uniform(0, 10, size=(n_agents, 2))
-    # floor the coords and calculate neighborhood numbers
-    floored = np.floor(locations).astype(int)
-    x_coords = floored[:, 0]
-    y_coords = floored[:, 1]
-    # we have 100 1x1 neighborhoods in a 10x10 grid
-    # start from 0-9 on the bottom row, all the way to 90-99 on the top row
-    # so every cell number when labelled like this ends with the floor of the x coord and begins with the floor of the y coord
-    neighborhood = y_coords * 10 + x_coords   
+    n_agents = agents.size
 
     houses_datatype = np.dtype([
         ("id", np.int32),
@@ -25,28 +16,29 @@ def initialize_houses(agents):
     houses = np.zeros(n_agents, houses_datatype) 
     houses["id"] = np.arange(n_agents)
     houses["tenant"] = np.full(n_agents, -1)
-    houses["neighborhood"] = neighborhood
+    houses["neighborhood"] = allocate_neighborhood(agents)
     # TESTING THE VALUE VARIABLE TO CHECK HOMELESSNESS
-    houses["value"] = np.full(n_agents, 1_00_000) # assuming median 2bhk price is 30k/m, so 3.6L pa
+    houses["value"] = np.full(n_agents, STARTING_HOUSE_PRICE) # assuming median 2bhk price is 30k/m, so 3.6L pa
     return houses
 
 "--------------------------------------- populate the houses column in agents ---------------------------------------"
 @njit(cache = True)
 def agent_house_mapping(agents, houses):
-    n_agents = agents.shape[0]
-    n_houses = houses.shape[0]
+    n_houses = houses.size
     # we set everyone to -1 and then map their houses later
     # otherwise we run into problems with negative indexing for homeless people
+    agents["neighborhood"][:] = -1
     agents["house"][:] = -1
     for h in range(n_houses):
-        t = houses["tenant"][h]
+        t = houses["tenant"][h] # agent id living in home h
         if t != -1: # we dont assign homeless tenants houses
-            agents["house"][t] = h
+            agents["house"][t] = h # agent t lives in home h
+            agents["neighborhood"][t] = houses["neighborhood"][h] # matches agent and home's neighborhoods
     return
 "--------------------------------- check if an agent can no longer afford their home --------------------------------"
 @njit(parallel = True, cache = True)
-def check_priced_out(agents, houses, proportions, β=0.3, λ=0.2, δ=0.6):
-    n_agents = agents.shape[0]
+def check_priced_out(agents, houses, proportions, β=BETA, λ=LAMBDA, δ=DELTA):
+    n_agents = agents.size
     priced_out_mask = np.zeros(n_agents, dtype=np.bool_)
 
     for i in prange(n_agents):
@@ -67,7 +59,7 @@ def check_priced_out(agents, houses, proportions, β=0.3, λ=0.2, δ=0.6):
 
 "---------------------------------- evict the poor dudes who are now priced out :( ----------------------------------"
 def evict_priced_out(agents, houses, priced_out_mask):
-    n_agents = agents.shape[0]
+    n_agents = agents.size
     for i in range(n_agents):
         h = agents["house"][i] # corresponds to house id they live in
         if priced_out_mask[i]:
@@ -76,12 +68,12 @@ def evict_priced_out(agents, houses, priced_out_mask):
                 agents["house"][i] = -1
                 agents["neighborhood"][i] = -1
                 agents["rent_paid"][i] = 0.0 # you don't pay rent if you're homeless
+
+    agent_house_mapping(agents,houses)
     return
 "---------------------------------------- decide which agent gets which house ---------------------------------------"
 @njit(cache=True)
-def allocate_houses(agents, houses, bids, neighborhood_chosen, β=0.3, λ=0.2, δ=0.4):
-    n_agents = agents.shape[0]
-    n_houses = houses.shape[0]
+def allocate_houses(agents, houses, bids, neighborhood_chosen):
     n_neighborhoods = np.max(houses["neighborhood"])+1
     cutoff_bids = np.zeros(n_neighborhoods)
     vacant_mask = houses["tenant"] == -1
@@ -110,10 +102,11 @@ def allocate_houses(agents, houses, bids, neighborhood_chosen, β=0.3, λ=0.2, �
         # give winners their vacant homes
         for w, v in zip(winners, vacancies):
             houses["tenant"][v] = w
-            houses["value"][v] = bids[w]
             agents["house"][w] = v
-            agents["rent_paid"][w] = bids[w]
             agents["neighborhood"][w] = n
+            # all houses share the same price: the market clearing price
+            houses["value"][v] = cutoff_bids[n]
+            agents["rent_paid"][w] = cutoff_bids[n]
 
     agent_house_mapping(agents, houses) # update the mapping after the allocation is made
     return agents, houses, cutoff_bids
@@ -141,3 +134,13 @@ def update_prices(houses, neighborhood_chosen, cutoff_bids,
         houses["value"][mask] = np.clip(new_price, a_min = old_price * (1-max_change), a_max = old_price * (1+max_change))
  
     return houses
+
+"--------------------------------- get an array of current rent in each neighborhood --------------------------------"
+def get_current_rents(houses, n_neighborhoods=N_NEIGHBORHOODS):
+    current_rents = np.zeros(n_neighborhoods)
+    for i in range(n_neighborhoods):
+        # select all houses in neighborhood i
+        mask = houses['neighborhood'] == i
+        # since all houses have the same rent, rent of neighborhood i = rent of first house in neighborhood i
+        current_rents[i] = houses['value'][np.where(mask)[0][0]]
+    return current_rents